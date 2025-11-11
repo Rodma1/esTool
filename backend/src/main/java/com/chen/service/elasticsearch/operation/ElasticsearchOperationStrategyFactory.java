af66b6a8de0a38eeb925ea18eff2062cddca129f
chengan <1156028692@qq.com>
2025-11-11 15:19:14 +0800

    feat: 添加分析器查询和解析

--- backend/src/main/java/com/chen/service/elasticsearch/operation/ElasticsearchOperationStrategyFactory.java
+++ backend/src/main/java/com/chen/service/elasticsearch/operation/ElasticsearchOperationStrategyFactory.java
@@ -26,6 +26,8 @@ public class ElasticsearchOperationStrategyFactory {
                 return new IndexTemplateOperationStrategy(factoryParam);
             case "TASK":
                 return new TaskOperationStrategy(factoryParam);
+            case "ANALYZE":
+                return new AnalyzeOperationStrategy(factoryParam);
             case "OTHER":
                 return new EsOther();
             default:
